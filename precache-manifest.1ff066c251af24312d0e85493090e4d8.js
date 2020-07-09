edd67a141e18baec6217e42da6ad3544d88c7945
Natalya Myunster <natalyamyunster@yahoo.com>
2020-07-09 08:58:58 +0200

    Updates

---
+++ precache-manifest.1ff066c251af24312d0e85493090e4d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6b4adabd2abe0ea12415864a83ba1d",
+    "url": "/WL_React_photo-gallery/index.html"
+  },
+  {
+    "revision": "bdc64a3608291bd1122f",
+    "url": "/WL_React_photo-gallery/static/css/2.bb342caa.chunk.css"
+  },
+  {
+    "revision": "0a15eb8bfedb2f4d196d",
+    "url": "/WL_React_photo-gallery/static/css/main.ee83c3cf.chunk.css"
+  },
+  {
+    "revision": "bdc64a3608291bd1122f",
+    "url": "/WL_React_photo-gallery/static/js/2.cfd89816.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WL_React_photo-gallery/static/js/2.cfd89816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a15eb8bfedb2f4d196d",
+    "url": "/WL_React_photo-gallery/static/js/main.c6cc218e.chunk.js"
+  },
+  {
+    "revision": "428f30038eeb1642dc23",
+    "url": "/WL_React_photo-gallery/static/js/runtime-main.a2aa5356.js"
+  }
+]);
